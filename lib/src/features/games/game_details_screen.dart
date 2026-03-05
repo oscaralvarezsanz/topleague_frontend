0188d1655b61b315f06ddcda698db31474f3274d
oscaralvarezs <oscar.alvarezs@estudiante.uam.es>
2026-03-05 20:42:45 +0100

    Fix typo in game_details_screen.dart

--- lib/src/features/games/game_details_screen.dart
+++ lib/src/features/games/game_details_screen.dart
@@ -1,4 +1,4 @@
-43import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../../models/dto/game_dto.dart';
 import '../../models/dto/participation_dto.dart';
